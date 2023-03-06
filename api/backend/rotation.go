e236b5e9eebbf64811d3aa2242c766d8e154d61b
Luo Xiaoyu <565451092@qq.com>
2023-03-06 22:30:13 +0800

    创建轮播图

--- api/backend/rotation.go
+++ api/backend/rotation.go
@@ -5,7 +5,10 @@ import (
 )
 
 type RotationReq struct {
-	g.Meta `path:"/backend/rotation/add" tags:"Rotation" method:"post" summary:"You first rotation api"`
+	g.Meta `path:"/backend/rotation/add" tags:"轮播图" method:"post" summary:"创建轮播图接口"`
+	PicUrl string `json:"pic_url"    v:"required#图片链接不能为空" dc:"图片链接"`
+	Link   string `json:"link"    v:"required#跳转链接不能为空" dc:"跳转链接"`
+	Sort   int    `json:"sort"    dc:"排序"`
 }
 type RotationRes struct {
 	RotationId uint `json:"rotationId"`
---
+++ internal/controller/rotation.go
@@ -0,0 +1,27 @@
+package controller
+
+import (
+	"context"
+	"gframe-shop-v2/api/backend"
+	"gframe-shop-v2/internal/model"
+	"gframe-shop-v2/internal/service"
+)
+
+// Rotation 内容管理
+var Rotation = cRotation{}
+
+type cRotation struct{}
+
+func (a *cRotation) Create(ctx context.Context, req *backend.RotationReq) (res *backend.RotationRes, err error) {
+	out, err := service.Rotation().Create(ctx, model.RotationCreateInput{
+		RotationCreateUpdateBase: model.RotationCreateUpdateBase{
+			PicUrl: req.PicUrl,
+			Link:   req.Link,
+			Sort:   req.Sort,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &backend.RotationRes{RotationId: out.RotationId}, nil
+}
