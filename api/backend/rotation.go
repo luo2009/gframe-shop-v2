8226d033d690c2097695f451ab0cffd030ccc20a
Luo Xiaoyu <565451092@qq.com>
2023-03-21 23:47:55 +0800

    列表取值统一输出标准_以轮播图模块举例

--- api/backend/rotation.go
+++ api/backend/rotation.go
@@ -28,3 +28,16 @@ type RotationUpdateReq struct {
 	Sort   int    `json:"sort"    dc:"排序"`
 }
 type RotationUpdateRes struct{}
+
+type RotationGetListCommonReq struct {
+	g.Meta `path:"/backend/rotation/list" method:"get" tags:"轮播图" summary:"轮播列表图接口"`
+	Sort   int `json:"sort"   in:"query" dc:"排序类型"`
+	CommonPaginationReq
+}
+
+type RotationGetListCommonRes struct {
+	List  interface{} `json:"list" description:"列表"`
+	Page  int         `json:"page" description:"分页码"`
+	Size  int         `json:"size" description:"分页数量"`
+	Total int         `json:"total" description:"数据总数"`
+}
